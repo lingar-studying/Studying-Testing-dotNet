1753c1628144506f30025f3ed538beefc9d3f6d2
lingar <yimprogramming@gmail.com>
2025-03-26 09:10:55 +0200

    Saving

---
+++ ConsoleApp1/users/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingTesting.users
+{
+    public class User
+    {
+        private string username;
+        private List<User_Role> roles;
+
+        public List<User_Role> Roles { get => roles; set => roles = value; }
+        public string Username { get => username; set => username = value; }
+    }
+}
