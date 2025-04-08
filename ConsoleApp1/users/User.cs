a0a83818a8f20d5287ba27470219113bced55409
lingar <yimprogramming@gmail.com>
2025-04-09 02:46:36 +0300

    Saving code

--- ConsoleApp1/users/User.cs
+++ ConsoleApp1/users/User.cs
@@ -1,5 +1,4 @@
-﻿using StudyingTesting.game_area;
-using StudyingTesting.poker_hands.game_area;
+﻿using StudyingTesting.poker_hands.game_area;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +10,13 @@ namespace StudyingTesting.users
     public class User
     {
         private string username;
-        private List<User_Role> roles;
+        private List<User_Role> roles = new List<User_Role>();
 
         private List<Table> tables = new List<Table>();
 
         public List<User_Role> Roles { get => roles; set => roles = value; }
         public string Username { get => username; set => username = value; }
+        public List<Table> Tables { get => tables; set => tables = value; }
 
         public void AddTable()
         {
