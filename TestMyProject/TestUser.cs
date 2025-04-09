acdd2234ba82b07c0ed215cb7b882e2a5b4c3379
Oded <oded.frig1@gmail.com>
2025-04-09 19:51:14 +0300

    oded.

--- TestMyProject/TestUser.cs
+++ TestMyProject/TestUser.cs
@@ -57,6 +57,57 @@ namespace TestMyProject
             //Assert.AreEqual(Expected, c.Number);
         }
 
+        [TestMethod]
+        public void TestHighCard()
+        {
+            Card card1 = new Card(2, Suit.CLUBS);
+            Card card2 = new Card(5, Suit.HEARTS);
+            Card card3 = new Card(7, Suit.SPADES);
+            Card card4 = new Card(9, Suit.DIAMONDS);
+            Card card5 = new Card(11, Suit.CLUBS);
+
+            Hand hand = new Hand();
+            hand.Cards = new Card[] { card1, card2, card3, card4, card5 };
+
+            WinningRank rank = hand.FindWinningRank();
+
+            Assert.AreEqual(WinningRank.HIGH, rank, "The hand should be High Card when no pair is present.");
+        }
+
+        [TestMethod]
+        public void TestPair()
+        {
+            Card card1 = new Card(2, Suit.CLUBS);
+            Card card2 = new Card(2, Suit.HEARTS);
+            Card card3 = new Card(7, Suit.SPADES);
+            Card card4 = new Card(9, Suit.DIAMONDS);
+            Card card5 = new Card(11, Suit.CLUBS);
+
+            Hand hand = new Hand();
+            hand.Cards = new Card[] { card1, card2, card3, card4, card5 };
+
+            WinningRank rank = hand.FindWinningRank();
+
+            Assert.AreEqual(WinningRank.PAIR, rank, "The hand should be identified as a Pair.");
+        }
+
+        [TestMethod]
+        public void TestPairDoesNotBecomeHighCard()
+        {
+            Card card1 = new Card(3, Suit.CLUBS);
+            Card card2 = new Card(3, Suit.HEARTS);  // This makes it a Pair
+            Card card3 = new Card(7, Suit.SPADES);
+            Card card4 = new Card(9, Suit.DIAMONDS);
+            Card card5 = new Card(11, Suit.CLUBS);
+
+            Hand hand = new Hand();
+            hand.Cards = new Card[] { card1, card2, card3, card4, card5 };
+
+            WinningRank rank = hand.FindWinningRank();
+
+            Assert.AreEqual(WinningRank.PAIR, rank, "A hand with a pair should not be considered a High Card.");
+        }
+
 
 
     }
